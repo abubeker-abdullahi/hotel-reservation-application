78cea90f2f26fe1aa2b7ae67ab231f3616b43cdc
Abubeker Abdullahi <abubeker.abdullahi1@gmail.com>
2024-07-03 20:54:27 +0000

    Added javadocs to api

--- app/src/main/java/api/AdminResource.java
+++ app/src/main/java/api/AdminResource.java
@@ -1,46 +1,62 @@
 package api;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 
 import model.Customer;
-import model.Reservation;
 import model.IRoom;
+import service.CustomerService;
+import service.ReservationService;
 
+/**
+ * The AdminResource class provides an API for administrative tasks related to the hotel.
+ * This includes functionalities for managing customers, rooms, and reservations.
+ */
 public class AdminResource {
-
-    static final Collection<Customer> customersList = new HashSet<>();
-    static final Collection<IRoom> roomsList = new HashSet<>();
-    static final Collection<Reservation> reservationsList = new HashSet<>();
-
+    
+    /**
+     * Retrieves a customer by their email.
+     *
+     * @param email The email of the customer.
+     * @return The customer with the given email, or null if no such customer exists.
+     */
     public static Customer getCustomer(String email) {
-        for (Customer customer : customersList) {
-            if (customer.getEmail().equals(email)) {
-                return customer;
-            }
-        }
-        return null;
+        return CustomerService.getCustomer(email);
     }
 
+    /**
+     * Adds a list of rooms to the hotel.
+     *
+     * @param rooms The list of rooms to be added.
+     */
     public static void addRoom(List<IRoom> rooms) {
         for (IRoom room : rooms) {
-            roomsList.add(room);
+            ReservationService.addRoom(room);
         }
     }
 
+    /**
+     * Retrieves all rooms in the hotel.
+     *
+     * @return A collection of all rooms.
+     */
     public static Collection<IRoom> getAllRooms() {
-        return new HashSet<>(roomsList);
+        return ReservationService.getAllRooms();
     }
 
+    /**
+     * Retrieves all customers of the hotel.
+     *
+     * @return A collection of all customers.
+     */
     public static Collection<Customer> getAllCustomers() {
-        return new HashSet<>(customersList);
+        return CustomerService.getAllCustomers();
     }
 
-    public static void displayAllReservation() {
-        for (Reservation reservation : reservationsList) {
-            System.out.println(reservation);
-        }
+    /**
+     * Displays all reservations in the hotel.
+     */
+    public static void displayAllReservations() {
+        ReservationService.printAllReservation();
     }
 }
-
--- app/src/main/java/api/HotelResource.java
+++ app/src/main/java/api/HotelResource.java
@@ -1,5 +1,6 @@
 package api;
 
+import java.text.ParseException;
 import java.util.Collection;
 import java.util.Date;
 
@@ -9,35 +10,76 @@ import model.Reservation;
 import service.CustomerService;
 import service.ReservationService;
 
+/**
+ * The HotelResource class provides an API for accessing hotel-related services.
+ * This includes functionalities for retrieving customers, rooms, and reservations.
+ */
 public class HotelResource {
 
+    /**
+     * Retrieves a customer by their email.
+     *
+     * @param email The email of the customer.
+     * @return The customer with the given email, or null if no such customer exists.
+     */
     public static Customer getCustomer(String email) {
         return CustomerService.getCustomer(email);
     }
 
+    /**
+     * Creates a new customer.
+     *
+     * @param email     The email of the new customer.
+     * @param firstName The first name of the new customer.
+     * @param lastName  The last name of the new customer.
+     */
     public static void createACustomer(String email, String firstName, String lastName) {
        CustomerService.addCustomer(email, firstName, lastName);
     }
 
+    /**
+     * Retrieves a room by its room number.
+     *
+     * @param roomNumber The room number.
+     * @return The room with the given room number, or null if no such room exists.
+     */
     public static IRoom getRoom(String roomNumber) {
         return ReservationService.getARoom(roomNumber);
     }
 
-    public static Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate) {
+    /**
+     * Books a room for a customer.
+     *
+     * @param customerEmail The email of the customer.
+     * @param room          The room to be booked.
+     * @param checkInDate   The check-in date.
+     * @param checkOutDate  The check-out date.
+     * @return The reservation details, or null if the room cannot be booked.
+     * @throws ParseException if there is an error parsing the date.
+     */
+    public static Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate) throws ParseException {
         Customer customer = getCustomer(customerEmail);
-
-        if (customer == null || room == null || checkInDate == null || checkOutDate == null) {
-            throw new IllegalArgumentException("Invalid input. Please provide valid customer email, room, check-in date, and check-out date.");
-        }
 
         return ReservationService.reserveARoom(customer, room, checkInDate, checkOutDate);
     }
 
-
+    /**
+     * Retrieves all reservations for a given customer.
+     *
+     * @param customerEmail The email of the customer.
+     * @return A collection of reservations for the customer, or an empty collection if none exist.
+     */
     public static Collection<Reservation> getCustomerReservations(String customerEmail) {
         return ReservationService.getCustomerReservation(getCustomer(customerEmail));
     }
 
+    /**
+     * Finds available rooms for a given date range.
+     *
+     * @param checkIn  The check-in date.
+     * @param checkOut The check-out date.
+     * @return A collection of available rooms for the specified date range.
+     */
     public static Collection<IRoom> findARoom(Date checkIn, Date checkOut) {
         return ReservationService.findRooms(checkIn, checkOut);
     }
